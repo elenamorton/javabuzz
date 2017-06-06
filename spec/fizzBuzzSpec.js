02937c5eaed9a4c3a8deeea1e5bad497a42317ba
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-06-06 10:42:15 +0100

    JS fizzbuzz solution.

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,54 @@
+describe('Javabuzz', function() {
+    var javabuzz;
+    beforeEach(function() {
+        javabuzz = new Javabuzz();
+    });
+    
+    describe('knows when a number is', function() {
+        
+        it('divisible by 3', function() {
+             expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+        });
+    });
+    
+    describe('knows when a number is NOT', function() {
+        
+        it('divisible by 3', function() {
+            expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+        });
+    });
+    
+    describe('knows when a number is', function() {
+        
+        it('divisible by 5', function() {
+            expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+        });
+    });
+    
+    describe('knows when a number is NOT', function() {
+        
+        it('divisible by 5', function() {
+            expect(javabuzz.isDivisibleByFive(2)).toBe(false);
+        });
+    });
+    
+    describe('knows when a number is', function() {
+        
+        it('divisible by 15', function() {
+            expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+        });
+    });
+    
+    describe('knows when a number is NOT', function() {
+        
+        it('divisible by 15', function() {
+            expect(javabuzz.isDivisibleByFifteen(9)).toBe(false);
+        });
+    });
+    
+    describe('when playing, says', function() {
+       it('Java when a number is divisible by 3', function() {
+           expect(javabuzz.says(3)).toEqual("Java");
+       }) ;
+    });
+});
